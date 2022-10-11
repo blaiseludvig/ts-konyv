bb9766c065d9baa326d78788c0ae5088d795857e
blaiseludvig <blaiseludvig@gmail.com>
2022-10-11 21:12:15 +0200

    próba futás hozzáadása

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ import './style.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 
-console.log("javascript betöltve!");
+let element: HTMLHeadingElement = document.createElement('h1');
+element.textContent = "Ha ezt látod, és zöld, akkor működik!";
+// a text-success egy bootstrapes stílus
+element.className = "text-success";
+document.body.append(element);
