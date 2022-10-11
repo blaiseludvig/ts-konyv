e4abfc85fce6a54b64cb4e2884c71c69bced996f
blaiseludvig <blaiseludvig@gmail.com>
2022-10-11 20:09:03 +0200

    package.json és config fájlok létrehozása

---
+++ webpack.config.cjs
@@ -0,0 +1,28 @@
+const webpack = require("webpack");
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const config = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  optimization: {
+    minimize: false,
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+  ],
+};
+
+module.exports = config;
